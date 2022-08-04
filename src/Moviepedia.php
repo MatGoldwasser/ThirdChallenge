f3e36b2cdac885e624d8cdcb0e7aced66afd28be
Mateo Goldwasser <mateo.goldwasser@lightit.io>
2022-08-04 14:08:56 -0300

    Table ready

--- src/Moviepedia.php
+++ src/Moviepedia.php
@@ -3,8 +3,10 @@
 namespace Name;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use GuzzleHttp\Client;
 
@@ -15,7 +17,8 @@ class Moviepedia extends Command
     {
         $this->setName('show')
              ->setDescription('Get information about movie')
-             ->addArgument('movie', InputArgument::REQUIRED);
+             ->addArgument('movie', InputArgument::REQUIRED)
+             ->addOption('fullPlot', null, InputOption::VALUE_OPTIONAL, 'Display the movie full plot', false);
 
     }
 
@@ -31,11 +34,31 @@ class Moviepedia extends Command
                         't' => $movie]
         ]);
 
-        $contents = $response->getBody()->getContents();
+        $response1 = $client->request('GET', 'http://www.omdbapi.com/', [
+            'query' => ['apikey' => $api,
+                        't' => $movie,
+                        'plot' => 'full']
+        ]);
+
+        //Falta esto, como saber si puso fullPlot o no
+        if($input->getOption('fullPlot')){
+            $contents = $response1->getBody()->getContents();
+        }else{
+            $contents = $response->getBody()->getContents();
+        }
+
         $movie_info = json_decode($contents, true);
 
-        $output->writeln("<info>{$movie_info["Year"]}</info>");
-
+        if(strcmp($movie_info["Response"], "False") == 0){
+            $output->writeln("<info>That movie doesn't exist!</info>");
+        }else{
+            $table = new Table($output);
+            $table->setHeaders(['Clave', 'Valor']);
+            $output->writeln("<info>{$movie} - {$movie_info["Year"]}</info>");
+            foreach($movie_info as $clave=>$valor){
+                $table->addRow([strval($clave),strval($valor)]);
+            }
+            $table->render();
+        }
     }
-
 }
